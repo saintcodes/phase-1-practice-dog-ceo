a5d5def83b240bf7a4c04a3feadfb5915006bc9f
Michelle <clairsin@gmail.com>
2022-05-04 20:08:40 -0400

    commit tree 056d6105977c546db366e3de902dd798d58683c2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(res => res.json())
+  .then(data => {
+    const images = data.message
+    images.forEach(image => {
+      let img = document.createElement('img')
+      let dogImage = document.createElement('ul')
+      document.body.append(dogImage)
+      img.src = image
+      dogImage.append(img)
+  })
+})
+
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(data => {
+      const breeds = data.message
+      const dogBreed = document.querySelector('#dog-breeds')
+      // console.log(breeds)
+      const breedNames = Object.keys(breeds)
+      // console.log(breedNames)
+      for (const breeds of breedNames) {
+        let li = document.createElement('li')
+        dogBreed.append(li)
+        li.innerHTML = breeds
+        li.addEventListener('click', changesColor)
+      }
+    })
+
+function changesColor(e) {
+  let randomColor = Math.floor(Math.random()*16777215).toString(16)
+  e.target.style.color = "#"+randomColor
+}
+
+//create function where dropdown letter equal to first letter of each
+//word in the list "onchange"
